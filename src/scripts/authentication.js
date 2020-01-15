69e49f538345026c95dbfeed3afeb99838e298c2
giovsteph <giov.ponce@gmail.com>
2020-01-15 11:59:43 -0600

    twitter login enabled

--- src/scripts/authentication.js
+++ src/scripts/authentication.js
@@ -26,25 +26,25 @@ firebase.auth().onAuthStateChanged(function(user) {
 //Github authentication
 var providerGit = new firebase.auth.GithubAuthProvider();
 
-//Facebook authentication
-var providerFacebook = new firebase.auth.FacebookAuthProvider();
+//Twitter authentication
+var providerTwitter = new firebase.auth.TwitterAuthProvider();
 
 //Google authentication
 var providerGoogle = new firebase.auth.GoogleAuthProvider();
 
-const loginWithProvider = (provider) =>{
+const loginWithProvider = (provider) => {
     welcomeScreen.setAttribute("style", "display: none;");
     loader.setAttribute("style", "display: Flex;"); //Flex
-    switch(provider){
+    switch (provider) {
         case 1:
             firebase.auth().signInWithRedirect(providerGoogle);
-        break;
+            break;
         case 2:
             firebase.auth().signInWithRedirect(providerGit);
-        break;
+            break;
         case 3:
-            firebase.auth().signInWithRedirect(providerFacebook);
-        break;
+            firebase.auth().signInWithRedirect(providerTwitter);
+            break;
     }
 }
 
@@ -52,8 +52,9 @@ firebase.auth().getRedirectResult()
     .then(function(result) {
         if (result.user != null) {
             //TODO: fix blinker
+            console.log(result.user)
             profileCreation(result.user.displayName, result.user.email, result.user.photoURL);
-            
+
         }
     }).catch(function(error) {
         welcomeScreen.setAttribute("style", "display: block;");
@@ -69,39 +70,39 @@ firebase.auth().getRedirectResult()
             formErrorMsj.innerHTML = "An account already exists with the same email but different sign-in credentials.";
             loginFormErrorMsj.innerHTML = "An account already exists with the same email but different sign-in credentials.";
         } else {
-            alert("Ocurrio un error en la autenticación [Loggin with social network].");
+            alert("Ocurrio un error en la autenticación [Login with social network].");
         }
     });
 
 //Registration with email
-const emailRegistration = (userEmail, userPassword,userName) => {
+const emailRegistration = (userEmail, userPassword, userName) => {
     firebase.auth().createUserWithEmailAndPassword(userEmail, userPassword)
-                .then(function() {
-                    //TODO: uid con Email authentication
-                    let user = firebase.auth().currentUser;
-                    profileCreation(userName, user.email, null);
+        .then(function() {
+            //TODO: uid con Email authentication
+            let user = firebase.auth().currentUser;
+            profileCreation(userName, user.email, null);
 
-                }).catch(function(error) {
-                    // Handle Errors here.
-                    welcomeScreen.setAttribute("style", "display: block;");
-                    loader.setAttribute("style", "display: none;"); //Flex
-                    profileScreen.setAttribute("style", "display:none;");
-                    var errorCode = error.code;
-                    var errorMessage = error.message;
-                    console.error("Error " + errorCode + ": " + errorMessage);
-                    if (errorCode == "auth/email-already-in-use") {
-                        formErrorMsj.setAttribute("style", "visibility: visible;");
-                        formErrorMsj.innerHTML = "The email address is already in use by another account.";
-                    } else if (errorCode == "auth/invalid-email") {
-                        formErrorMsj.setAttribute("style", "visibility: visible;");
-                        formErrorMsj.innerHTML = "Invalid Email Address."
-                    } else if (errorCode == "auth/weak-password") {
-                        formErrorMsj.setAttribute("style", "visibility:visible;");
-                        formErrorMsj.innerHTML = "Password should be at least 6 characters."
-                    } else {
-                        alert("Ocurrio un error en la autenticación [Email account creation].");
-                    }
-                });
+        }).catch(function(error) {
+            // Handle Errors here.
+            welcomeScreen.setAttribute("style", "display: block;");
+            loader.setAttribute("style", "display: none;"); //Flex
+            profileScreen.setAttribute("style", "display:none;");
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.error("Error " + errorCode + ": " + errorMessage);
+            if (errorCode == "auth/email-already-in-use") {
+                formErrorMsj.setAttribute("style", "visibility: visible;");
+                formErrorMsj.innerHTML = "The email address is already in use by another account.";
+            } else if (errorCode == "auth/invalid-email") {
+                formErrorMsj.setAttribute("style", "visibility: visible;");
+                formErrorMsj.innerHTML = "Invalid Email Address."
+            } else if (errorCode == "auth/weak-password") {
+                formErrorMsj.setAttribute("style", "visibility:visible;");
+                formErrorMsj.innerHTML = "Password should be at least 6 characters."
+            } else {
+                alert("Ocurrio un error en la autenticación [Email account creation].");
+            }
+        });
 }
 
 //Login  with email/password
@@ -135,7 +136,7 @@ const signOut = () => {
     firebase.auth().signOut().then(function() {
         // Sign-out successful.
         userProfilePicture.setAttribute("src", "src//assets//imgs//avatar128.png");
-        
+
         userProfileName.innerHTML = "";
         userProfileEmail.innerHTML = "";
     }).catch(function(error) {
