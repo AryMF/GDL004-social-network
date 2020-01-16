80a97b57eddddc285955a275cb4ea315d20e827d
AryMF <ariadne.hp@gmail.com>
2020-01-16 10:58:14 -0600

    Stable version code review day

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -3,13 +3,21 @@ const dataSource = firebase.firestore();
 //dataSource.settings ({ timestampsInSnapshots }); //TODO: research this
 
 //Registro de datos del usuario  en base de datos
-const profileCreation = (name, email, picture) => {
+/*const profileCreation = (name, email, picture) => {
     let profileInfo = {
         displayName: name,
         email: email,
         profilePicture: picture
     }
     dataSource.collection("user").doc(email).set(profileInfo).then(function() {
+        console.log("Document successfully written!");
+    });
+}*/
+
+const profileCreation = (_profileInfo) => {
+    console.log("profileCreation");
+    console.log("_profileInfo");
+    dataSource.collection("user").doc(_profileInfo.email).set(_profileInfo).then(function() {
         console.log("Document successfully written!");
     });
 }
